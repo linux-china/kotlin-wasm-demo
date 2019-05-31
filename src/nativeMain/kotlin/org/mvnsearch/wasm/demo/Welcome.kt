627e04265f512a7fa5f7ffed5331735f6c86e459
linux_china <libing.chen@gmail.com>
2019-05-31 13:57:44 -0700

    Welcome class

---
+++ src/nativeMain/kotlin/org/mvnsearch/wasm/demo/Welcome.kt
@@ -0,0 +1,13 @@
+package org.mvnsearch.wasm.demo
+
+/**
+ * Welcome class
+ *
+ * @author linux_china
+ */
+class Welcome {
+
+    fun hello(): String {
+        return "Hello Kotlin WASM!";
+    }
+}
