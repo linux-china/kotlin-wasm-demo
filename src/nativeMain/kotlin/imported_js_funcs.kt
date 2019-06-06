498d118067da1aa437f5f3553413fda9f149ae5b
linux_china <libing.chen@gmail.com>
2019-06-05 17:57:01 -0700

    add JsString

--- src/nativeMain/kotlin/imported_js_funcs.kt
+++ src/nativeMain/kotlin/imported_js_funcs.kt
@@ -3,8 +3,7 @@
  *
  * @author linux_china
  */
-import kotlinx.wasm.jsinterop.stringLengthBytes
-import kotlinx.wasm.jsinterop.stringPointer
+import kotlinx.wasm.jsinterop.*
 
 fun alert(text: String) {
     knjs__alert(stringPointer(text), stringLengthBytes(text))
@@ -12,3 +11,20 @@ fun alert(text: String) {
 
 @SymbolName("knjs__alert")
 external fun knjs__alert(textPtr: Int, textLen: Int): Unit
+
+
+@SymbolName("Konan_js_getStringChar")
+external fun getStringChar(arena: Arena, obj: Object, index: Int): Int
+
+open class JsString(arena: Arena, index: Object) : JsValue(arena, index) {
+    constructor(jsValue: JsValue) : this(jsValue.arena, jsValue.index)
+
+    fun getString(): String {
+        val length = this.getInt("length")
+        var result = ""
+        for (i in 0..length - 1) {
+            result += getStringChar(arena, index, i).toChar()
+        }
+        return result;
+    }
+}
